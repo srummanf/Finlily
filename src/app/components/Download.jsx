3f3e87c67e1e65ace7cdc4ba9d58e1a3e07f171a
Rumman <rummanfardeen4567@gmail.com>
2024-10-05 17:26:14 +0530

    ⚡️Fixed UI ✨

--- src/app/components/Download.jsx
+++ src/app/components/Download.jsx
@@ -4,7 +4,7 @@ const Download = () => {
   return (
     
 
-<div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+<div class="w-full p-4 text-center bg-white border border-gray-200 shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
     <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Work fast from anywhere</h5>
     <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Stay up to date and move work forward with Flowbite on iOS & Android. Download the app today.</p>
     <div class="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4 rtl:space-x-reverse">
--- src/app/components/Pricing.jsx
+++ src/app/components/Pricing.jsx
@@ -1,180 +1,129 @@
-import React from 'react'
+import React from "react";
 
 const Pricing = () => {
   return (
-      <section className="dark:bg-[#292929] dark:text-gray-100">
-          <div className="container mx-auto p-6 overflow-x-auto">
-              <table className="w-full">
-                  <caption className="sr-only">Pricing plan comparison</caption>
-                  <thead>
-                      <tr>
-                          <th></th>
-                          <th scope="col">
-                              <h2 className="px-2 text-lg font-medium">Free User</h2>
-                              <p className="mb-3">
-                                  <span className="text-2xl font-bold sm:text-4xl dark:text-gray-50">0€</span>
-                                  <span className="font-medium dark:text-gray-400">/mo</span>
-                              </p>
-                          </th>
-                          <th scope="col">
-                              <h2 className="px-2 text-lg font-medium">Pro User</h2>
-                              <p className="mb-3">
-                                  <span className="text-2xl font-bold sm:text-4xl dark:text-gray-50">19€</span>
-                                  <span className="font-medium dark:text-gray-400">/mo</span>
-                              </p>
-                          </th>
-                          
-                      </tr>
-                  </thead>
-                  <tbody className="space-y-6 text-center divide-y dark:divide-gray-700">
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Past Performance of our calls</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Top gainers of the day</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
+    <section className="bg-white">
+      <div className="container mx-auto p-6 overflow-x-auto">
+        <h1 className="text-3xl font-bold text-center mb-8 text-black">
+          Price Plans
+        </h1>
+        <table className="w-full">
+          <caption className="sr-only">Pricing plan comparison</caption>
+          <thead>
+            <tr>
+              <th></th>
+              <th scope="col">
+                <h2 className="px-2 text-lg font-medium text-black">
+                  Free User
+                </h2>
+                <p className="mb-3">
+                  <span className="text-2xl font-bold sm:text-4xl text-black">
+                    0€
+                  </span>
+                  <span className="font-medium text-gray-500">/mo</span>
+                </p>
+              </th>
+              <th scope="col">
+                <h2 className="px-2 text-lg font-medium text-black">
+                  Pro User
+                </h2>
+                <p className="mb-3">
+                  <span className="text-2xl font-bold sm:text-4xl text-black">
+                    19€
+                  </span>
+                  <span className="font-medium text-gray-500">/mo</span>
+                </p>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-center divide-y divide-gray-300">
+            {[
+              {
+                feature: "Past Performance of our calls",
+                free: true,
+                pro: true,
+              },
+              { feature: "Top gainers of the day", free: true, pro: true },
+              { feature: "Top Losers of the day", free: true, pro: true },
+              { feature: "Daily Updates of the market", free: true, pro: true },
+              { feature: "Stock calls / Advisory", free: false, pro: true },
+              {
+                feature: "Analysis of stock called or an advised stocks",
+                free: false,
+                pro: true,
+              },
+              {
+                feature: "Advanced Insights and Analytics",
+                free: false,
+                pro: true,
+              },
+              { feature: "Dedicated Support", free: false, pro: true },
+            ].map(({ feature, free, pro }, index) => (
+              <tr key={index}>
+                <th scope="row" className="text-left py-3 text-black">
+                  <h3>{feature}</h3>
+                </th>
+                <td>
+                  {free ? (
+                    <svg
+                      className="w-5 h-5 mx-auto text-green-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="w-5 h-5 mx-auto text-red-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  )}
+                </td>
+                <td>
+                  {pro ? (
+                    <svg
+                      className="w-5 h-5 mx-auto text-green-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="w-5 h-5 mx-auto text-red-500"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </section>
+  );
+};
 
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Top Losers of the day</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Daily Updates of the market</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Stock calls / Advisory</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Not included in Free plan" className="w-5 h-5 mx-auto dark:text-gray-600">
-                                  <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Analysis of stock called or an advised stocks</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Not included in Free plan" className="w-5 h-5 mx-auto dark:text-gray-600">
-                                  <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Daily updates of the stock calls</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Not included in Free plan" className="w-5 h-5 mx-auto dark:text-gray-600">
-                                  <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Short Term stock</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Not included in Free plan" className="w-5 h-5 mx-auto dark:text-gray-600">
-                                  <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                      <tr>
-                          <th scope="row" className="text-left">
-                              <h3 className="py-3">Stock of the month (Long Term Stocks)</h3>
-                          </th>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Not included in Free plan" className="w-5 h-5 mx-auto dark:text-gray-600">
-                                  <path fillRule="evenodd" d="M3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-                          <td>
-                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-label="Included in Standard plan" className="w-5 h-5 mx-auto dark:text-blue-400">
-                                  <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd"></path>
-                              </svg>
-                          </td>
-
-                      </tr>
-                  </tbody>
-              </table>
-          </div>
-      </section>  
-  )
-}
-
-export default Pricing
+export default Pricing;
