da385d4eb6c569625b0cf6f0a495a83e267420ef
Rumman <rummanfardeen4567@gmail.com>
2024-10-07 09:33:43 +0530

    ⚒️Colors fixed⚒️

--- src/app/components/Pricing.jsx
+++ src/app/components/Pricing.jsx
@@ -4,7 +4,6 @@ import HalfYearly from "../public/images/halfyearly.png";
 import Yearly from "../public/images/yearly.png";
 import Image from "next/image";
 
-
 const Pricing = () => {
   return (
     <section className="py-6 bg-white  text-black">
@@ -18,7 +17,7 @@ const Pricing = () => {
           </p>
         </div>
         <div className="grid max-w-md grid-cols-1 gap-6 mx-auto auto-rows-fr lg:max-w-full lg:gap-2 xl:gap-6 lg:grid-cols-3">
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow  ">
             <a href="https://finlilytechnology.com/stock-calls/">
               <Image
                 className="p-8 rounded-t-lg"
@@ -28,7 +27,7 @@ const Pricing = () => {
             </a>
             <div className="px-5 pb-5">
               <a href="https://finlilytechnology.com/stock-calls/">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                   Stock Calls - Half Yearly Plan
                 </h5>
               </a>
@@ -71,7 +70,7 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                    className="w-4 h-4 text-gray-200 "
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -80,20 +79,20 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                 </div>
-                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded  ms-3">
                   5.0
                 </span>
               </div>
               <div className="flex items-center justify-between">
-                <span className="text-xl font-bold text-gray-500 dark:text-white line-through">
+                <span className="text-xl font-bold text-gray-500  line-through">
                   ₹5,500
                 </span>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                <span className="text-3xl font-bold text-gray-900 ">
                   ₹4,499
                 </span>
                 <a
                   href="https://finlilytechnology.com/stock-calls/"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                 >
                   Add to cart
                 </a>
@@ -101,7 +100,7 @@ const Pricing = () => {
             </div>
           </div>
 
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
             <a href="https://finlilytechnology.com/stock-calls/">
               <Image
                 className="p-8 rounded-t-lg"
@@ -111,7 +110,7 @@ const Pricing = () => {
             </a>
             <div className="px-5 pb-5">
               <a href="https://finlilytechnology.com/stock-calls/">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                   Stock Calls - Quaterly Plan
                 </h5>
               </a>
@@ -154,7 +153,7 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                    className="w-4 h-4 text-gray-200 "
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -163,20 +162,20 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                 </div>
-                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded  ms-3">
                   5.0
                 </span>
               </div>
               <div className="flex items-center justify-between">
-                <span className="text-xl font-bold text-gray-500 dark:text-white line-through">
+                <span className="text-xl font-bold text-gray-500  line-through">
                   ₹3,500
                 </span>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                <span className="text-3xl font-bold text-gray-900 ">
                   ₹2,499
                 </span>
                 <a
                   href="https://finlilytechnology.com/stock-calls/"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                 >
                   Add to cart
                 </a>
@@ -184,7 +183,7 @@ const Pricing = () => {
             </div>
           </div>
 
-          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
             <a href="https://finlilytechnology.com/stock-calls/">
               <Image
                 className="p-8 rounded-t-lg"
@@ -194,7 +193,7 @@ const Pricing = () => {
             </a>
             <div className="px-5 pb-5">
               <a href="https://finlilytechnology.com/stock-calls/">
-                <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
                   Stock Calls - Yearly Plan
                 </h5>
               </a>
@@ -237,7 +236,7 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                   <svg
-                    className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                    className="w-4 h-4 text-gray-200 "
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="currentColor"
@@ -246,20 +245,18 @@ const Pricing = () => {
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                   </svg>
                 </div>
-                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded  ms-3">
                   5.0
                 </span>
               </div>
               <div className="flex items-center justify-evenly">
-                <span className="text-xl font-bold text-gray-500 dark:text-white line-through">
+                <span className="text-xl font-bold text-gray-500  line-through">
                   ₹10,000
                 </span>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  ₹7499
-                </span>
+                <span className="text-3xl font-bold text-gray-900 ">₹7499</span>
                 <a
                   href="https://finlilytechnology.com/stock-calls/"
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
                 >
                   Add to cart
                 </a>
